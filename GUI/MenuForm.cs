d6ac598b876a905c7308afef3d4c5f8215d75b26
Smyth Lautaro Agustin <lautaroagustin.smyth@alumnos.uai.edu.ar>
2026-07-02 17:42:55 -0300

    T08: Verificacion periodica de integridad DVV en sesion activa (timer 5 min)

--- GUI/MenuForm.cs
+++ GUI/MenuForm.cs
@@ -1,4 +1,4 @@
-using BE;
+﻿﻿using BE;
 using BLL;
 using Abstracciones;
 using System;
@@ -15,7 +15,10 @@ namespace GUI
         private readonly ISessionManager _sessionManager = IoCContainer.Resolver<ISessionManager>();
         private readonly UsuarioBLL _usuarioBll = IoCContainer.Resolver<UsuarioBLL>();
         private readonly IManejadorIdioma _manejadorIdioma = IoCContainer.Resolver<IManejadorIdioma>();
+        private readonly IDigitoVerificadorService _dvServicio = IoCContainer.Resolver<IDigitoVerificadorService>();
         private bool _cargandoIdioma = false;
+        private bool _timerDisposed = false;
+        private System.Windows.Forms.Timer _timerIntegridad;
 
         public MenuForm()
         {
@@ -37,6 +40,11 @@ namespace GUI
             _timer.Interval = 1000;
             _timer.Start();
             ActualizarHora();
+
+            _timerIntegridad = new System.Windows.Forms.Timer();
+            _timerIntegridad.Interval = 300000;
+            _timerIntegridad.Tick += TimerIntegridad_Tick;
+            _timerIntegridad.Start();
         }
 
         private void CboIdioma_SelectedIndexChanged(object sender, EventArgs e)
@@ -88,9 +96,31 @@ namespace GUI
             ActualizarHora();
         }
 
+        private void TimerIntegridad_Tick(object sender, EventArgs e)
+        {
+            _timerIntegridad.Stop();
+            System.Collections.Generic.List<string> errores;
+            bool ok = _dvServicio.VerificarIntegridad(out errores);
+            if (!ok)
+            {
+                MessageBox.Show(
+                    "Se detectó una alteración en los datos del sistema. La sesión será cerrada por seguridad.",
+                    "Integridad comprometida",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                _usuarioBll.Logout(this.Text);
+                _timer.Stop();
+                if (!_timerDisposed) { _timer.Dispose(); _timerDisposed = true; }
+                _timerIntegridad.Dispose();
+                Application.Restart();
+                return;
+            }
+            _timerIntegridad.Start();
+        }
+
         private void ActualizarHora()
         {
-            lblHora.Text = "🕒 " + DateTime.Now.ToString("HH:mm:ss") + "  ";
+            lblHora.Text = "ðŸ•’ " + DateTime.Now.ToString("HH:mm:ss") + "  ";
         }
 
         private void BtnBitacora_Click(object sender, EventArgs e)
@@ -157,6 +187,9 @@ namespace GUI
                 }
                 _usuarioBll.Logout(this.Text);
                 _timer.Stop();
+                if (!_timerDisposed) { _timer.Dispose(); _timerDisposed = true; }
+                _timerIntegridad?.Stop();
+                _timerIntegridad?.Dispose();
             }
         }
 
@@ -226,6 +259,9 @@ namespace GUI
             {
                 _usuarioBll.Logout(this.Text);
                 _timer.Stop();
+                if (!_timerDisposed) { _timer.Dispose(); _timerDisposed = true; }
+                _timerIntegridad?.Stop();
+                _timerIntegridad?.Dispose();
                 Application.Restart();
             }
         }
@@ -234,10 +270,10 @@ namespace GUI
         {
             btnUsuarios.Text = _manejadorIdioma.ObtenerTexto("MenuForm.btnUsuarios");
             btnBitacora.Text = _manejadorIdioma.ObtenerTexto("MenuForm.btnBitacora");
-            btnPermisos.Text = _manejadorIdioma.ObtenerTexto("MenuForm.btnPermisos") ?? "🔑 Permisos";
-            btnCambios.Text = _manejadorIdioma.ObtenerTexto("MenuForm.btnCambios") ?? "📜 Cambios";
-            btnBackup.Text = _manejadorIdioma.ObtenerTexto("MenuForm.btnBackup") ?? "💾 Backup";
-            btnIdiomas.Text = _manejadorIdioma.ObtenerTexto("MenuForm.btnIdiomas") ?? "🌐 Idiomas";
+            btnPermisos.Text = _manejadorIdioma.ObtenerTexto("MenuForm.btnPermisos") ?? "ðŸ”‘ Permisos";
+            btnCambios.Text = _manejadorIdioma.ObtenerTexto("MenuForm.btnCambios") ?? "ðŸ“œ Cambios";
+            btnBackup.Text = _manejadorIdioma.ObtenerTexto("MenuForm.btnBackup") ?? "ðŸ’¾ Backup";
+            btnIdiomas.Text = _manejadorIdioma.ObtenerTexto("MenuForm.btnIdiomas") ?? "ðŸŒ Idiomas";
             btnCerrarSesion.Text = _manejadorIdioma.ObtenerTexto("MenuForm.btnCerrarSesion");
             lblUsuario.Text = $"{_manejadorIdioma.ObtenerTexto("MenuForm.lblSesionInfo")} {_usuarioBll.ObtenerUsernameEnSesion()}";
             lblBaseDatos.Text = $"{_manejadorIdioma.ObtenerTexto("MenuForm.lblServidorInfo")} {_conexionService.ObtenerNombreBaseDatos()}";
